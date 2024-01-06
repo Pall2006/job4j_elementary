14f35c31670085a4d9e2571464b4a4cb93a13cc9
Pall2006 <pall2006@bk.ru>
2024-01-06 17:49:48 +0300

    Фиксация изменений на github [#243564 #470843]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("My name is Pall");
+        System.out.println("My name is Pavel");
         System.out.println("03.05.1980");
     }
 
