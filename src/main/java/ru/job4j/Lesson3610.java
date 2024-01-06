e4fdbf594d8e4a1eeddc5a9ecf1e2b6e4aa0d970
Pall2006 <pall2006@bk.ru>
2024-01-06 11:07:27 +0300

    Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/Lesson3610.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Lesson3610 {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+
+    }
+}
