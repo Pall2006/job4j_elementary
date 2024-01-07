a44ce0eab0754352eeee8d59e015c4fb3c23b776
Pall2006 <pall2006@bk.ru>
2024-01-07 07:44:48 +0300

    Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/ArgumentsMethod.java
+++ src/main/java/ru/job4j/calculator/ArgumentsMethod.java
@@ -2,7 +2,6 @@ package ru.job4j.calculator;
 
 public class ArgumentsMethod {
     public static void hello(String name, int age) {
-
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
