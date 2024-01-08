6e16ef4f8b33792815b494e6cd7fb566eed9584b
Pall2006 <pall2006@bk.ru>
2024-01-08 07:01:53 +0300

    Конвертер валюты [#41583]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -7,13 +7,11 @@ package ru.job4j.calculator;
  */
 public class Fit {
     public static double manWeight(short height) {
-        double result = (height - 100) * 1.15;
-        return result;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double result = (height - 110) * 1.15;
-        return result;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
