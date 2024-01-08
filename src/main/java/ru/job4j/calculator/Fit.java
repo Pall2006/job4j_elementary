4cc0018a3cbb87c6be9dfdf234919095e1345580
Pall2006 <pall2006@bk.ru>
2024-01-08 06:53:23 +0300

    Конвертер валюты [#41583]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+/**
+ * Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15.
+ *
+ * Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
+ */
+public class Fit {
+    public static double manWeight(short height) {
+        double result = (height - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = (height - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short heightOfMan = 187;
+        double man = Fit.manWeight(heightOfMan);
+        System.out.println("Man 187 is " + man);
+
+        short heightOfWoman = 175;
+        double woman = Fit.manWeight(heightOfWoman);
+        System.out.println("Woman 175 is " + woman);
+    }
+
+}
+
