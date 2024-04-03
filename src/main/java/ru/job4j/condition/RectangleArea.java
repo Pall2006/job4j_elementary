70424a959502f51007e0fef047984d7ce55eb53e
Pall2006 <pall2006@bk.ru>
2024-04-03 13:22:50 +0300

    rect

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -12,7 +12,7 @@ public class RectangleArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        return l * h;
+        return l + h;
     }
 
     public static void main(String[] args) {
