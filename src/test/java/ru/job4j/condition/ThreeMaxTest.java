bc37181707601173a0201c11614cdb164eb636a2
Pall2006 <pall2006@bk.ru>
2024-04-03 15:40:15 +0300

    Отладка программы в IDEA [#159464 #489607]

--- src/test/java/ru/job4j/condition/ThreeMaxTest.java
+++ src/test/java/ru/job4j/condition/ThreeMaxTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ThreeMaxTest {
+
     @Test
     void firstMax() {
         int first = 10;
