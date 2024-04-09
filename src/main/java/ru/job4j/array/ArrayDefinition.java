1d16adf4918bd45af9bd425c4a78f080cdcc8e6b
Pall2006 <pall2006@bk.ru>
2024-04-09 20:08:09 +0300

    Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,4 +5,12 @@ public class ArrayDefinition {
     String[] surname = new String[100500];
     float[] prices = new float[40];
 
+    public static void main(String[] args) {
+        ArrayDefinition arrayDefinition = new ArrayDefinition();
+
+        // Выводим размер каждого массива в консоль
+        System.out.println("Размер массива ages: " + arrayDefinition.ages.length);
+        System.out.println("Размер массива surname: " + arrayDefinition.surname.length);
+        System.out.println("Размер массива prices: " + arrayDefinition.prices.length);
+    }
 }
