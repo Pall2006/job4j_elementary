8d4115c71608d680b54791a954940e31586d71f4
Pall2006 <pall2006@bk.ru>
2024-04-10 10:33:21 +0300

    Размер массива [#333582 #490678]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,15 +7,8 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         ArrayDefinition arrayDefinition = new ArrayDefinition();
-
-        String[] names = new String[4];
-        names[0] = "Ivan Ivanov";
-        names[1] = "Petr Petrov";
-        names[2] = "Sidr Sidorov";
-        names[3] = "Сан Саныч";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        System.out.println("Размер массива ages: " + arrayDefinition.ages.length);
+        System.out.println("Размер массива surname: " + arrayDefinition.surname.length);
+        System.out.println("Размер массива prices: " + arrayDefinition.prices.length);
     }
 }
