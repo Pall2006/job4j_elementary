004c00c1869115e4fb5446ede7d88e1d862717eb
Pall2006 <pall2006@bk.ru>
2024-04-11 12:44:51 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        for (int index = 0; index < prefix.length; index++) {
+            if (word[index] == prefix[index]) {
+                continue;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
