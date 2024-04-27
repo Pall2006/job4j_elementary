37e3d7cfe9a03c7a2d109e481312de8e29bdb727
Pall2006 <pall2006@bk.ru>
2024-04-27 15:38:48 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/test/java/ru/job4j/condition/MaximumNamberTest.java
+++ src/test/java/ru/job4j/condition/MaximumNamberTest.java
@@ -1,16 +1,15 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import ru.job4j.condition.Max;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class MaxTest {
+class MaximumNamberTest {
     @Test
     void whenMax1To2Then2() {
         int left = 1;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = MaximumNamber.max(left, right);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
@@ -19,7 +18,7 @@ class MaxTest {
     void whenMax2To5Then5() {
         int left = 5;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = MaximumNamber.max(left, right);
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
@@ -28,7 +27,7 @@ class MaxTest {
     void whenEqual2And2Then2() {
         int left = 2;
         int right = 2;
-        int result = Max.max(left, right);
+        int result = MaximumNamber.max(left, right);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
